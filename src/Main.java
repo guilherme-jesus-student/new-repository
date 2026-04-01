bc4f920980fc20e1935be2139ab2dafd82bcc026
guilhermehenryquejesus <guilhermehenryque.jesus@gmail.com>
2026-04-01 14:58:54 -0300

    Melhorando a classe main

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        Cliente cliente = new Cliente("Mãe", "119999999");
-        Profissional prof = new Profissional("Ana Karina", "118888888", "Manicure");
-        Servico servico = new Servico("Unha em gel", 80);
-
-        Cliente cliente2 = new Cliente("Guilherme", "117777777");
-        Profissional prof2 = new Profissional("Ana Karina", "116666666", "Manicure");
-        Servico servico2 = new Servico("Manicure", 50);
-
-
-        Agendamento ag = new Agendamento(cliente, prof, servico, "10:00");
-        Agendamento ag2 = new Agendamento(cliente2, prof2, servico2, "11:00");
         Agenda agenda = new Agenda();
 
-        agenda.adicionarAgendamento(ag);
-        agenda.adicionarAgendamento(ag2);
-
-        agenda.cancelarPorHorario("10:00");
-
-        agenda.listarAgendamentos();
+        try {
+            executarSistema(agenda);
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
+
+    private static void executarSistema(Agenda agenda) {
+
+        Cliente cliente = new Cliente("Guilherme", "117777777");
+        Profissional prof = new Profissional("Ana", "118888888", "Manicure");
+        Servico servico = new Servico("Unha", 50);
+
+        Agendamento ag = new Agendamento(cliente, prof, servico,
+                LocalDateTime.of(2026, 4, 1, 10, 0));
+
+        agenda.adicionarAgendamento(ag);
+
+        agenda.listarAgendamentos().forEach(System.out::println);
+    }
+}
